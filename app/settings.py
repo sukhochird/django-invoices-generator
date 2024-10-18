4e52649079778c93a57bf9fc673edeb648c08343
dsukhochir <dsukhochir@gmail.com>
2024-10-18 17:54:46 +0800

    server

--- app/settings.py
+++ app/settings.py
@@ -163,8 +163,8 @@ if not DEBUG:
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_ROOT = 'media/'
-
+# MEDIA_ROOT = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
